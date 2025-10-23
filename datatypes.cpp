2718950a34c49af713f8b590c0ae9724f7138bc7
Deepthi Kolipaka <deepthikolipakaaa@gmail.com>
2025-10-24 05:16:46 +0530

    Add files via upload

---
+++ datatypes.cpp
@@ -0,0 +1,57 @@
+/*#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int x;
+    cin >> x ;
+    cout <<"this is the value of x:"<< x <<"\n";//int varies upto 10^-9 to 10^9
+    return 0;
+
+}
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    long c;
+    cin >> c;
+    cout << c; // long int varies b/w 10^-12- 10^12
+    return 0;
+}
+#include<bits/stdc++.h>
+uisng namespace std;
+int main(){
+    long long d;
+    cin>> d;
+    cout << d; // long long stores b/1 10^-15 to 10 ^15
+    return 0;
+} 
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    float r;
+    cin >> r; //float number means with decimals 
+    cout << r;
+    return 0;
+}
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    double y;
+    cin >> y;
+    cout << y;
+    return 0;
+}*/
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+string s;
+cin >> s;
+cout << s; //first word only prints
+return 0;
+}
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string s;
+    getline(cin,s); // which prints whole line text 
+    cout << s;
+    return 0;
+}
