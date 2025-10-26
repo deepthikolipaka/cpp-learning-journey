8f09817d21779f89dcbd30fc3b64908eda62cdcb
Deepthi Kolipaka <deepthikolipakaaa@gmail.com>
2025-10-27 04:34:13 +0530

    Add files via upload

---
+++ switch.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int d;
+    cin>>d;
+    switch(d){
+        case 1:
+        cout<<"monday";
+        break;
+        case 2:
+        cout<<"Tuesday";
+        break;
+        case 3:
+        cout<<"Wednesday";
+        break;
+        case 4:
+        cout<<"Thursday";
+        break;
+        case 5:
+        cout<<"friday";
+        break;
+        case 6:
+        cout<<"sat";
+        break;
+        case 7:
+        cout<<"sunday";
+        break;
+        default:
+        cout<<"its a invalid choice";
+        break;
+    }
+}
