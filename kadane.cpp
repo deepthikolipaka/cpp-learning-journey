1659f7c49646230373f2d284b6163b0462bf2a73
Deepthi Kolipaka <deepthikolipakaaa@gmail.com>
2026-01-04 09:00:24 +0530

    Add files via upload

---
+++ kadane.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+/*int main() {
+    int n;
+    cin >> n;
+    int arr[n];
+
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    int maxSum = arr[0], currSum = arr[0];
+
+    for (int i = 1; i < n; i++) {
+        currSum = max(arr[i], currSum + arr[i]);
+        maxSum = max(maxSum, currSum);
+    }
+
+    cout << maxSum;
+    return 0;
+}*/
